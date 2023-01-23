ae1707e13e78371076729d5a3dd1399ef91c004f
gregnboat <greigpaais@gmail.com>
2023-01-23 18:57:18 +0900

    Making Intro Activity as a starter.

--- app/src/main/java/com/greig/foodery/project39/Activity/MainActivity.java
+++ app/src/main/java/com/greig/foodery/project39/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.greig.foodery;
+package com.greig.foodery.project39.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.greig.foodery.R;
 
 public class MainActivity extends AppCompatActivity {
 
