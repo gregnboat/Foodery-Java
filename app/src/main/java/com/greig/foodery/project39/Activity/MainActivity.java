0d6a0adcc110bded4749eee77a2df31df78ed4ee
gregnboat <greigpaais@gmail.com>
2023-01-25 11:40:15 +0900

    Making main menu part 3 (Backend code).

--- app/src/main/java/com/greig/foodery/project39/Activity/MainActivity.java
+++ app/src/main/java/com/greig/foodery/project39/Activity/MainActivity.java
@@ -1,16 +1,28 @@
 package com.greig.foodery.project39.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.greig.foodery.R;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
     }
 }
---
+++ app/src/main/java/com/greig/foodery/project39/Domain/CategoryDomain.java
@@ -0,0 +1,11 @@
+package com.greig.foodery.project39.Domain;
+
+public class CategoryDomain {
+    private  String title;
+    private String pic;
+
+    public CategoryDomain(String title, String pic) {
+        this.title = title;
+        this.pic = pic;
+    }
+}
